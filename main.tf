60225a48cf4f66c5490d91d55d7406065f64ea69
PramodBandgar <pramodjb2002@gmail.com>
2024-12-30 16:43:39 +0530

    Prequisites

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# Creating two EC2 instances using count
+resource "aws_instance" "Webserver" {
+  count         = 2  # Creates two instances
+  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID for Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Webserver-${count.index}"  # Unique name for each instance
+  }
+}
+
+output "instance_ids" {
+  value = aws_instance.example[*].id  # Outputs the IDs of both instances
+}
