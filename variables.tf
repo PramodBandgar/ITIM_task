c43eb16b490e95ef769896e2064ac801ffe9e785
PramodBandgar <pramodjb2002@gmail.com>
2024-12-30 17:33:52 +0530

    variables

---
+++ variables.tf
@@ -0,0 +1,18 @@
+# Declare the AWS Access Key and Secret Key variables
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
