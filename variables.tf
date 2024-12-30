19512fcd351c2c631aee12f1023a976bbf9ea6a9
PramodBandgar <pramodjb2002@gmail.com>
2024-12-30 17:58:54 +0530

    region removed

--- variables.tf
+++ variables.tf
@@ -11,8 +11,8 @@ variable "secret_key" {
   sensitive   = true
 }
 
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+#provider "aws" {
+# region     = "ap-south-1"
+#  access_key = var.access_key
+#  secret_key = var.secret_key
+#}
